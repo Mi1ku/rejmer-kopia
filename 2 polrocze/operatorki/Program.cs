910698f214ff92c8198a92ee0052208a52676fcc
Mikus <72542227+Mi1ku@users.noreply.github.com>
2026-01-15 12:00:06 +0100

    es

---
+++ 2 polrocze/operatorki/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+// Zad 1 
+int a = 2;
+int b = 3;
+int pole = a * b;
+Console.WriteLine(pole);
+
+// Zad 2
+int l1 = 6;
+int l2 = 7;
+int l3 = 8;
+int srednia = (l1 + l2 + l3) / 3;
+Console.WriteLine(srednia);
+
+// Zad 3
+int lj1 = 6;
+int lj2 = 4;
+int resztazdzielenia = lj1 % lj2;
+Console.WriteLine(resztazdzielenia);
+
+// Zad 4
+int liczba10 = 10;
+liczba10 += 5;
+Console.WriteLine(liczba10);
+
+// Zad 5
+int liczba4 = 4;
+liczba4 *= 3;
+Console.WriteLine(liczba4);
+
+// Zad 6
+int liczba2 = 2;
+int wynikPotegi = (int)Math.Pow(liczba2, 3);
+Console.WriteLine(wynikPotegi);
+
+// Zad 7
+int wiek1 = 25;
+int wiek2 = 30;
+bool czyRowne = wiek1 == wiek2;
+Console.WriteLine("Czy wieki są równe? " + czyRowne);
+
+if (wiek1 > wiek2)
+{
+    Console.WriteLine("Pierwszy wiek jest większy.");
+}
+else if (wiek1 < wiek2)
+{
+    Console.WriteLine("Drugi wiek jest większy.");
+}
+else
+{
+    Console.WriteLine("Oba wieki są takie same.");
+}
+
+// Zad 8
+int dowolna = 15;
+bool czyWPrzedziale = dowolna >= 10 && dowolna <= 20;
+Console.WriteLine($"Czy liczba {dowolna} mieści się w przedziale [10, 20]? {czyWPrzedziale}");
+
+// Zad 9 
+int dowolnaliczba = 9;
+if (dowolnaliczba%2==0 && dowolnaliczba%3==0)
+{
+    Console.WriteLine($"Liczba {dowolnaliczba} jest podzielna przez 2 i 3");
+}
+else
+{
+    Console.WriteLine($"Liczba {dowolnaliczba} nie jest podzielna przez 2 i 3");
+}
+
+// Zad 10
+bool czy_Pada = true;
+czy_Pada = !czy_Pada;
+if  (czy_Pada == true)
+{
+    Console.WriteLine("Pada");
+}
+else
+{
+    Console.WriteLine("Nie pada");
+}
---
+++ 2 polrocze/zmienne-typy-danych/Program.cs
@@ -0,0 +1,80 @@
+﻿// Zad 1
+int wiek = 18;
+string imie = "Mikołaj";
+Console.WriteLine($"Mam {wiek} lat i nazywam sie {imie}");
+// Zad 2
+int liczba1 = 20;
+int liczba2 = 2;
+Console.WriteLine(liczba1 + liczba2);
+Console.WriteLine(liczba1 - liczba2);
+Console.WriteLine(liczba1 * liczba2);
+Console.WriteLine(liczba1 / liczba2);
+// Zad 3
+int calk = 20;
+calk += 30;
+calk -= 12;
+calk *= 2;
+calk /= 2;
+Console.WriteLine(calk);
+// Zad 4
+bool pada = true;
+if (pada == true)
+{
+    Console.WriteLine("Pada deszcz");
+}
+else
+{
+    Console.WriteLine("Nie pada deszcz");
+}
+// Zad 5
+int metry = 20;
+int m_to_cm = metry * 100;
+Console.WriteLine($"Wynik w centymetrach to: {m_to_cm}");
+// Zad 6
+string imie2 = "Mikołaj";
+string powitanie = "Witaj";
+Console.WriteLine($"{powitanie} {imie2}");
+// Zad 7
+string tekst = "mikolaj to SiGmA SkIbIDi!";
+Console.WriteLine(tekst.ToLower());
+Console.WriteLine(tekst.ToUpper());
+// Zad 8 
+string miasto = "Darłowo";
+Console.WriteLine(miasto.Substring(0, miasto.Length - 4));
+// Zad 9 
+string zdanie = "jebać policje";
+string szukane_slowo = "jebać";
+if (zdanie.Contains(szukane_slowo))
+{
+    Console.WriteLine($"Zdanie: {zdanie}, zawiera słowo {szukane_slowo}");
+}
+else
+{
+    Console.WriteLine($"Zdanie nie zawiera szukanego slowa!");
+}
+// Zad 10
+string zdanie2 = "kocham male kotki";
+string noweZdanie = zdanie2.Replace("male", "duze");
+
+Console.WriteLine("Po zmianie: " + noweZdanie);
+// Ćw 11
+string dlugitekst = "Maksa boli brzuch";
+Console.WriteLine($"Ten tekst ma: {dlugitekst.Length} znaków");
+// Ćw 12
+string ciagcyfr = "19998271721";
+Console.WriteLine(ciagcyfr.Length);
+// Ćw 13
+string tekst1 = "Ala ma kota";
+int liczbaa1 = 2;
+float liczbaa2 = 3.5f;
+Console.WriteLine(tekst1.GetType());
+Console.WriteLine(liczbaa1.GetType());
+Console.WriteLine(liczbaa2.GetType());
+// Ćw 14
+int liczba5 = 20;
+string liczba6 = liczba5.ToString();
+Console.WriteLine(liczba6.GetType());
+// Ćw 15
+
+
+
