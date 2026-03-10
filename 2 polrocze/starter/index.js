c649a5b17ec0386305b1f8688d2e937d69a2d873
Mikus <72542227+Mi1ku@users.noreply.github.com>
2026-03-10 14:13:52 +0100

    es

--- 2 polrocze/starter/index.js
+++ 2 polrocze/starter/index.js
@@ -1,21 +1,36 @@
 const fs = require('fs');
+const http = require('http');
 
+/*
+<----------------------------------------->
+                Files
+<----------------------------------------->
 
-// // Blokowanie synchronicznej drogi
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-// const textOut = `Kocham Avocado i jestem z tego dumny. ${textIn} \nStworzone ${new Date().toLocaleDateString()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('Plik został zapisany');
+// Blokowanie synchronicznej drogi
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+const textOut = `Kocham Avocado i jestem z tego dumny. ${textIn} \nStworzone ${new Date().toLocaleDateString()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('Plik został zapisany');
 
 // Asynchroniczna droga
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+fs.readFile('./txt/startttt.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('Błąd ⛔');
+
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         console.log(data2);
         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
             console.log(data3);
             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Plik został 💽!');
+            });
         });
     });
 }
-);
+)
+console.log('Wczytywanie 🗃️...');
+*/
+// <----------------------------------------->
+//                Server
+// <----------------------------------------->
+
