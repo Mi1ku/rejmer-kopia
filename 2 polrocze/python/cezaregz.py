3d5a71b51f07a12d891ad60220bfb68c7473d50d
Mikus <72542227+Mi1ku@users.noreply.github.com>
2026-03-18 13:01:54 +0100

    es

--- 2 polrocze/python/cezaregz.py
+++ 2 polrocze/python/cezaregz.py
@@ -1,15 +1,27 @@
-litery = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-zaszyfrowane_litery = []
+def szyfr_cezara(tekst_jawny: str, klucz: int) -> str:
+    tekst_zaszyfrowany = ""
+    klucz = klucz % 26
 
-def szyfr_cezara(tekst, klucz):
-    for litera in tekst:
-        if litera in litery:
-            index = litery.index(litera)
-            zaszyfrowany_index = (index + klucz) % klucz 
-            zaszyfrowane_litery.append(litery[zaszyfrowany_index])
+    for znak in tekst_jawny:
+        if znak == " ":
+            tekst_zaszyfrowany += " "
         else:
-            zaszyfrowane_litery.append(litera)
-    return "".join(zaszyfrowane_litery)
+            przesuniety = ord(znak) + klucz
 
-tekst = input("Podaj tekst do zaszyfrowania: ")
-klucz = int(input("Podaj klucz: "))
+            if przesuniety > ord('z'):
+                przesuniety -= 26
+
+            if przesuniety < ord('a'):
+                przesuniety += 26
+
+            tekst_zaszyfrowany += chr(przesuniety)
+
+    return tekst_zaszyfrowany
+
+def main():
+    print("Szyfr Cezara")
+    tekst_jawny = input("Podaj tekst jawny: ")
+    klucz = int(input("Podaj klucz (liczba całkowita): "))
+
+    wynik = szyfr_cezara(tekst_jawny, klucz)
+    print(f"Tekst zaszyfrowany: {wynik}")
