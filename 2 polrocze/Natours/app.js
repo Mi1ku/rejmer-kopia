f3cc3d2827cff9b1205f91d88053c27bb6de7e6e
Mikus <72542227+Mi1ku@users.noreply.github.com>
2026-04-09 14:13:49 +0200

    es

--- 2 polrocze/Natours/app.js
+++ 2 polrocze/Natours/app.js
@@ -7,6 +7,11 @@ const express = require('express');
 */
 const app = express();
 const port = 3000;
+
+app.get('/',() => {
+    
+});
+
 app.listen(port, () => {
     console.log(`Serwer działa na porcie ${port} 🚀`);
 });
