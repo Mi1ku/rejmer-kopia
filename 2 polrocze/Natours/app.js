4c7d91bcfd6f551d906cd76b3b676bdfb2cdf74d
Mikus <72542227+Mi1ku@users.noreply.github.com>
2026-04-10 14:13:56 +0200

    es

--- 2 polrocze/Natours/app.js
+++ 2 polrocze/Natours/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
-
 /*
 <----------------------------------------->
                 Express
 <----------------------------------------->
 */
+
 const app = express();
 const port = 3000;
 
-app.get('/',() => {
-    
+app.get('/', (req, res) => {
+    res
+        .status(200) 
+        .json({
+            message: 'Hello from the server side!',
+            app: 'Natours'
+        });
+});
+
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint...');
 });
 
 app.listen(port, () => {
