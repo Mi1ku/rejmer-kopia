04136432cbd28d45ce8a285fb62ddb50c7e9daf0
Mikus <72542227+Mi1ku@users.noreply.github.com>
2026-04-14 12:21:31 +0200

    es

--- 2 polrocze/Natours/app.js
+++ 2 polrocze/Natours/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+
 /*
 <----------------------------------------->
                 Express
@@ -7,20 +9,30 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/', (req, res) => {
-    res
-        .status(200) 
-        .json({
-            message: 'Hello from the server side!',
-            app: 'Natours'
-        });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello from the server side!',
+//     app: 'Natours'
+//   });
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tours
+    }
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Serwer działa na porcie ${port} 🚀`);
+  console.log(`Serwer działa na porcie ${port} 🚀`);
 });
