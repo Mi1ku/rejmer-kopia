4e276f73046864ccf755edee2a63defc45c2e544
Mikołaj Kuś <boydzburek@gmail.com>
2026-04-16 13:24:17 +0200

    es

---
+++ 2 polrocze/egz2026-stycz-zad2-powtorka/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace egz2026_stycz_zad2_powtorka
+{
+    internal class Program
+    {
+
+    }
+    abstract class Pytanie
+    {
+        protected string trescPytania, nazwa_Pliku;
+        protected bool czyPoprawna;  
+
+        public Pytanie(string trescPytania, string nazwa_Pliku, bool czyPoprawna)
+        {
+            this.trescPytania = trescPytania;
+            this.nazwa_Pliku = nazwa_Pliku;
+            this.czyPoprawna = czyPoprawna;
+        }
+        public abstract bool SprawdzOdpowiedz(char odpowiedz);
+    }
+    class PytanieZamkniete : Pytanie
+    {
+        private string odpA, odpB, odpC;
+        private char poprawnaOdp;
+        public PytanieZamkniete(string trescPytania, string nazwa_Pliku, string a, string b, string c, char czyPoprawna)
+            : base(trescPytania,nazwa_Pliku)
+        {
+            odpA = a;
+            odpB = b;
+            odpC = c;
+            czyPoprawna = poprawna;
+        }
+    }
+}
