7e5eea972a19139e5d2864c0ff11966caa4d9e39
Mikołaj Kuś <72542227+Mi1ku@users.noreply.github.com>
2026-04-24 14:13:12 +0200

    es

--- 2-polrocze/starter-test-nauka/serwer.js
+++ 2-polrocze/starter-test-nauka/serwer.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Serwer działa na porcie ${port} 🖥️`)
+})
+
 let items = [
     { id: 1, name: 'Element 1' },
     { id: 2, name: 'Element 2' },
     { id: 3, name: 'Element 3' }
 ]
 
+let nextId = 4;
 
-app.listen(port, () => {
-    console.log(`Serwer działa na porcie ${port} 🖥️`)
-})
-
+// get wszystkie items
 app.get('/items', (req, res) => {
     res.status(200).json({
         status: true,
@@ -20,17 +24,41 @@ app.get('/items', (req, res) => {
     });
 });
 
+// get item po id
 app.get('/items/:id', (req, res) => {
-    const { id } = req.params;
-    const item = items.find(item => item.id === parseInt(id));
-    if (!item) {
-        return res.status(404).json({
-            status: false,
-            message: 'Nie znaleziono elementu o id ' + id
-        });
-    }
-    res.status(200).json({
-        status: true,
-        data: item
-    });
+    const id = parseInt(req.params.id)
+    const item = items.find(el => el.id === id);
+
+    item ? res.json(item) : res.status(404).send('Nie znaleziono elementu');
 });
+
+// post dodaj
+app.post('/items', (req, res) => {
+    const newItem = {
+        id: nextId++,
+        name: req.body.name
+    }
+    items.push(newItem);
+    res.json(newItem);
+});
+
+// put edytuj
+app.put('/items/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const item = items.find(item => item.id === id);
+
+    if (item) {
+        item.name = req.body.name;
+        res.json(item);
+    } else {
+        res.status(404).send('Nie znaleziono elementu');
+    }
+});
+
+// delete usun
+app.delete('/items/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    items = items.filter(el => el.id !== id);
+
+    res.send("Usunieto")
+})
