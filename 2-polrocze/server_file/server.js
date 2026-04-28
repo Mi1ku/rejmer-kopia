a8d81e2c1efbcdbdb4a5614970b016d4d9856226
Mikołaj Kuś <72542227+Mi1ku@users.noreply.github.com>
2026-04-28 13:24:15 +0200

    es

--- 2-polrocze/server_file/server.js
+++ 2-polrocze/server_file/server.js
@@ -4,26 +4,50 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 const FILE = "server_file/dane.json";
-const data = readData();
+const dane = czytajDane();
 
-// pomocnicze funkcje
-function readData() {
+// uzywanie jsona w apce
+app.use(express.json());
+
+// pomocnicze funkcje - czytanie i zapisywanie danych 
+function czytajDane() {
    return JSON.parse(fs.readFileSync(FILE));
 }
 
-// gety
+function zapiszDane() {
+    fs.writeFileSync(FILE, JSON.stringify(dane, null, 2));
+}
+
+// gety - pobieranie elementów
+
 app.get('/dane', (req, res) => {
-    res.json(data);
+    res.json(dane);
 });
 
 app.get('/dane/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const item = data.find(element => element.id === id);
+    const item = dane.find(element => element.id === id);
     item 
         ? 
         res.json(item) 
         : 
-        res.status(404).send("Nie znaleziono itemu o podanym id!");
+        res.status(404).send("Nie znaleziono elementu o podanym id!");
+});
+
+// posty - dodawanie elementu 
+app.post('/dane', (req, res) => {
+    const newItem = {
+        id: dane.length++,
+        name: req.body.name
+    }
+    dane.push(newItem);
+    zapiszDane(dane);
+    res.json(newItem);
+});
+
+// puty -  edycja elementu
+app.put('/dane/:id', (req, res) => {
+    const id = parseInt(req.params.id);
 });
 
 // nasluchiwanie serwera
